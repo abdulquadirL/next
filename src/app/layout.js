62936236f41961afd761ce6dba7e19362634594e
buharry <elanquad92@gmail.com>
2024-03-26 09:27:23 +0100

    Add component and lib folders

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,8 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>This is the main layout</h1>
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
